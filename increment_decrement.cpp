8690b7440d3d4eef2cd58e506fbb0cd497a6bc1f
anji <anji_cruz@msn.com>
2023-10-17 01:21:37 +0800

    increment & decrement 10/17

---
+++ increment_decrement.cpp
@@ -0,0 +1,19 @@
+// increment & decrement
+#include <iostream>
+
+int main() 
+{
+    int a = 10;
+    int b = 10;
+    int c = 10;
+    int d = 10;
+    a++;
+    ++b;
+    --c;
+    d--;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+
+}
